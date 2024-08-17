bc65242f7a7c7a846a29a1ad16e0851a9c99b1b2
Sky Marsh <soras1758@gmail.com>
2024-08-17 19:25:19 +0900

    fixed index.js styling not affected. Add basic styling to sidebar

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
+import "./styles.css";
 import Project from "./project.js";
-import addNewProject from "./createNewProject.js";
 
 const defaultProject = new Project;
 defaultProject.name = "default";
 
 const projects = [defaultProject];
 
-function newProject(projectName){
+function addProject(projectName){
     const project = new Project;
     project.name = projectName;
     projects.push(project);
 }
-
-newProject("JP learning");
-debugger;
